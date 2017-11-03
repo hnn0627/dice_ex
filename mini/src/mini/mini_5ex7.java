53abbed93b96619810da8fb49bf7b3a465d875e3
PC <PC@DESKTOP-RNGGM1J>
2017-11-03 16:42:21 +0900

    ff

---
+++ mini/src/mini/mini_5ex7.java
@@ -0,0 +1,15 @@
+package mini;
+
+public class mini_5ex7 {
+	public static void main(String[] args) {
+	
+		int max = 0;
+		int [] array = {1,5,3,8,2};
+		for (int i =0; i<array.length;i++) {
+			if (array[i]>max)
+			max = array[i];
+		}
+		System.out.println("max :" +max);
+	}
+	
+}
