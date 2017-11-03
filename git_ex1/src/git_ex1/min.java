fa923d750c21e7c02bf1e512d668dd6ac75788a6
PC <PC@DESKTOP-RNGGM1J>
2017-11-03 17:40:55 +0900

    success

---
+++ git_ex1/src/git_ex1/min.java
@@ -0,0 +1,46 @@
+package git_ex1;
+
+public class min {
+		public static void main(String[] args) {
+
+	  int num = (int)(Math.random()*10);
+	  int ran2 = (int)(Math.random()*7);
+	  if(num==1) {
+		  System.out.println("1번이 나왔습니다.");
+	  }else if(num==2) {
+		  System.out.println("2번이 나왔습니다.");
+	  }else if(num==3) {
+		  System.out.println("3번이 나왔습니다.");
+	  }else if(num==4) {
+		  System.out.println("4번이 나왔습니다.");
+	  }else if(num==5) {
+		  System.out.println("5번이 나왔습니다.");
+	  }else if(num==6) {
+		  System.out.println("6번이 나왔습니다.");
+	  }else if(num == 0||num ==7||num ==8||num == 9) {
+		  System.out.println("범위에서 초과했습니다. 다시 굴립니다.");
+		  {
+	  if(ran2 == 1) {
+		  System.out.println("1번이 나왔습니다.");
+		  } else if(ran2 == 2) {
+		  System.out.println("2번이 나왔습니다.");
+		  } else if(ran2== 3) {
+		  System.out.println("3번이 나왔습니다.");
+		  } else if(ran2 == 4) {
+		  System.out.println("4번이 나왔습니다.");
+		  } else if(ran2 == 5) {
+		  System.out.println("5번이 나왔습니다.");
+		  } else if(ran2 == 6) {
+		  System.out.println("6번이 나왔습니다.");
+		  } 
+		  
+		  }
+	  }
+		}
+		 
+	  }
+	  
+	  
+	  
+
+
