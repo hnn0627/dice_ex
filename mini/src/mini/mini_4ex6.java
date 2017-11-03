fc3521a2449bae11be8cf7e8476ed3459e3876d6
PC <PC@DESKTOP-RNGGM1J>
2017-11-03 13:47:34 +0900

    test

---
+++ mini/src/mini/mini_4ex6.java
@@ -0,0 +1,14 @@
+package mini;
+
+public class mini_4ex6 {
+	public static void  main(String[] args) {
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i;j++) {
+		System.out.print("*");//ln(개인) <-제거 
+		if(j==i) {
+			System.out.println();
+		}
+	}
+}
+}
+}
